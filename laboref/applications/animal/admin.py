8bf841dbae5952bfce5b388f9559db494fee1847
plaboyano <programadorlaboyano@hotmail.com>
2020-07-05 21:42:31 -0500

    Se agrega la administraci?n de las clases de animales Con esto se puede verificar el funcionamiento de los modelos que componen la aplicaci?n animal. Se realiza la prueba de funcionamiento y su resultado es OK.

--- laboref/applications/animal/admin.py
+++ laboref/applications/animal/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from .models import AnimalType, Breed, AnimalColor, Animal
 
 
-"""
+admin.site.register(Animal)
 admin.site.register(AnimalType)
 admin.site.register(Breed)
 admin.site.register(AnimalColor)
-admin.site.register(Animal)
-"""
