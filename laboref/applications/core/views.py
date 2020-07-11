dcb5d4613377eb7f77e5d5693cb0d20c2ec86a5b
plaboyano <programadorlaboyano@hotmail.com>
2020-07-11 15:17:21 -0500

    Ajuste de interfaz home.html Se cargan los datos de los últimos animales registrados con el fin de colocarlo dinámicamente.

--- laboref/applications/core/views.py
+++ laboref/applications/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import ItemSlider, News
+from applications.animal.models import Animal
 
 """
 class IndexView(TemplateView):
@@ -21,11 +22,11 @@ class IndexListView(ListView):
 
     items = ItemSlider.objects.all()
     news = News.objects.all()
-
+    first_three_animals = Animal.objects.all().order_by('-created')[:3]
 
     def get_context_data(self, *, object_list=None, **kwargs):
 
         context = super(IndexListView, self).get_context_data(**kwargs)
-        context.update({'itemslider_list': self.items, 'news_list': self.news})
+        context.update({'itemslider_list': self.items, 'news_list': self.news, 'animals': self.first_three_animals})
         return context
 
